22f277b6bf5543933667fc5041ef779b87527657
Remi Escolano <127251203+MisterAirsoft@users.noreply.github.com>
2024-03-18 16:33:58 +0100

    Update Controller.java
    theme changer avec la librairie

--- src/Controller.java
+++ src/Controller.java
@@ -1,33 +1,26 @@
-import javax.swing.JFrame;
-import javax.swing.UIManager;
-
 import java.awt.Color;
-
 import javax.swing.*;
+import com.formdev.flatlaf.*;
 
-import com.formdev.flatlaf.FlatLaf;
-import com.formdev.flatlaf.FlatLightLaf;
-
-public class Controller {
+public class Controller extends javax.swing.JFrame {
+	public Controller(){
+		getComponents();
+	}
+	@SuppressWarnings("unchecked")
+	
 	public static void main(String[] args) {
 		
-		FlatLightLaf.setup();
-		UIManager.put( "TextComponent.arc", 5 );
-		UIManager.put( "TabbedPane.selectedBackground", Color.BLACK );
-		UIManager.put( "Component.arc", 0 );
-		UIManager.put( "ProgressBar.arc", 0 );
-		
-		// Création d'une fenêtre Swing
-        JFrame frame = new JFrame("Ma fenêtre");
-        frame.setSize(400, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FlatDarculaLaf.setup();
+		java.awt.EventQueue.invokeLater(new Runnable() {
 
-        //ajout barre de texte
-        JTextField textField = new JTextField("Barre de texte");
-        frame.add(textField);
-        
-        // Afficher la fenêtre
-        frame.setVisible(true);
+			@Override
+			public void run() {
+			
+				new Controller().setVisible(true);
+				
+			}
+			
+		});
+		
 	}
-	
 }
