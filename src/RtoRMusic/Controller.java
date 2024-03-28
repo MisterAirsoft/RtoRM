9003ad9e5f2e5942a639feb4731259540f228650
meriem-meryy <152922527+meriem-meryy@users.noreply.github.com>
2024-03-28 12:26:43 +0100

    Update Controller.java
    suppression des imports inutiles

--- src/RtoRMusic/Controller.java
+++ src/RtoRMusic/Controller.java
@@ -1,25 +1,18 @@
 package RtoRMusic;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import com.formdev.flatlaf.FlatDarculaLaf;
 
 public class Controller extends javax.swing.JFrame {
+	
 	public Controller(){
 		getComponents();
 	}
-	@SuppressWarnings("unchecked")
 	
 	public static void main(String[] args) {
-		Logger.getLogger("org.jaudiotagger").setLevel(Level.WARNING);
+		
 		FlatDarculaLaf.setup();
 		Vue vu1 = new Vue() ;
 		vu1.extractAndDisplayAlbumArt("Music", 4, 50);
-	
-			
 
-	
 		;
 		
 	}
