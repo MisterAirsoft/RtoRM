a1463422037591c4fc529b3361e84ed0f4770b0a
meriem-meryy <152922527+meriem-meryy@users.noreply.github.com>
2024-03-28 11:18:44 +0100

    Add files via upload
    LA CLASSE NoeudMusique EST IMPORTANTE

---
+++ src/RtoRMusic/NoeudMusique.java
@@ -0,0 +1,12 @@
+package RtoRMusic;
+
+
+public class NoeudMusique {
+    public Musique musique;
+    public NoeudMusique suivant;
+
+    public NoeudMusique(Musique musique) {
+        this.musique = musique;
+        this.suivant = null;
+    }
+}
