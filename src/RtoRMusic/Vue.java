5f89f517c8ac1dd1b2c5de7e4baf40c1c121526d
Remi Escolano <127251203+MisterAirsoft@users.noreply.github.com>
2024-04-03 15:39:23 +0200

    Update Vue.java
    correction partielle de la barre de recherche

--- src/RtoRMusic/Vue.java
+++ src/RtoRMusic/Vue.java
@@ -23,7 +23,7 @@ public class Vue {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         Font titleFont = new Font("Helvetica Neue Light", Font.ITALIC, 30); // Choisir la police, le style et la taille
-        Font titleFont1 = new Font("Helvetica Neue Light", Font.ITALIC, 30);
+        Font titleFont1 = new Font("Liberation Sans", Font.BOLD, 30);
       
 
         JPanel mainPanel = new JPanel(new BorderLayout());
@@ -33,46 +33,56 @@ public class Vue {
         searchField = new JTextField("Que souhaitez vouz écoutez ?",20);
         searchField.setBackground(Color.white);
         searchField.setFont(titleFont);
+        searchField.addMouseListener(new MouseAdapter() {
+
+            public void mouseClicked(MouseEvent evt) {
+            	searchField.setText("");
+            	searchField.setForeground(Color.black);
+            }
+        });
         
         JButton searchButton = new JButton("Rechercher");
+        
         // Ajout d'un écouteur pour changer la couleur du bouton lorsque survolé
         searchButton.addMouseListener(new MouseAdapter() {
+
             public void mouseEntered(MouseEvent evt) {
-                searchButton.setBackground(new Color(156, 208, 241)); // Couleur de fond légèrement plus claire
+                searchButton.setBackground(Color.gray); // Couleur de fond légèrement plus claire
             }
 
             public void mouseExited(MouseEvent evt) {
-                searchButton.setBackground(Color.WHITE); // Retour à la couleur de fond normale
+            	searchButton.setBackground(Color.white); // Retour à la couleur de fond normale
             }
         });
 
         // Définition des marges pour le bouton
         searchButton.setMargin(new Insets(5, 10, 5, 10)); 
-
+        searchButton.setSize(250,50 );
         searchButton.setBackground(Color.white);
         searchButton.setFont(titleFont1);
         Color titleColor1 = Color.black;
         searchButton.setForeground(titleColor1);
-        
+        JPanel musicPanel = new JPanel(new GridLayout(0, columns, 5, 5));
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String searchTerm = searchField.getText();
                 if (!searchTerm.isEmpty()) {
-                    m.rechercherMusique(searchTerm);
+                    afficherMusiques(musicPanel, play_list, imageSize, searchTerm);
                 } else {
-                    
+                    afficherMusiques(musicPanel, play_list, imageSize, ""); // Afficher toutes les musiques si la recherche est vide
                 }
             }
         });
+
         searchPanel.add(searchField);
         searchPanel.add(searchButton);
         mainPanel.add(searchPanel, BorderLayout.NORTH);
 
         // Affichage de la liste de musiques
-        JPanel musicPanel = new JPanel(new GridLayout(0, columns, 10, 10));
+        
         play_list = m.construire_play_list(folderPath);
-        afficherMusiques(musicPanel, play_list, imageSize);
+        afficherMusiques(musicPanel, play_list, imageSize,"");
         mainPanel.add(new JScrollPane(musicPanel), BorderLayout.CENTER);
 
         frame.getContentPane().add(mainPanel);
@@ -82,32 +92,43 @@ public class Vue {
         frame.setFont(titleFont);
     }
 
-    void afficherMusiques(JPanel panel, TreeSet<Musique> musiques, int imageSize) {
-        panel.removeAll(); // Nettoyer le panneau
-        for (Musique musique : musiques) {
-            ImageIcon imageIcon = new ImageIcon(musique.artwork.getBinaryData());
-            Image resizedImage = imageIcon.getImage().getScaledInstance(imageSize, imageSize, Image.SCALE_SMOOTH);
-            JLabel imageLabel = new JLabel(new ImageIcon(resizedImage));
-            imageLabel.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    	void afficherMusiques(JPanel panel, TreeSet<Musique> musiques, int imageSize, String searchTerm) {
+    	    panel.removeAll(); // Nettoyer le panneau
+    	    
+    	    for (Musique musique : musiques) {
+    	        // Vérifier si la musique correspond au terme de recherche
+    	        if (musique.titre.toLowerCase().contains(searchTerm.toLowerCase()) ||
+    	            musique.artist.toLowerCase().contains(searchTerm.toLowerCase()) ||
+    	            musique.album.toLowerCase().contains(searchTerm.toLowerCase()) ||
+    	            musique.genre.toLowerCase().contains(searchTerm.toLowerCase())) {
+    	            ImageIcon imageIcon = new ImageIcon(musique.artwork.getBinaryData());
+    	            Image resizedImage = imageIcon.getImage().getScaledInstance(imageSize, imageSize, Image.SCALE_SMOOTH);
+    	            JLabel imageLabel = new JLabel(new ImageIcon(resizedImage));
+    	            imageLabel.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-            JLabel titleLabel = new JLabel("Title: " + musique.titre);
-            JLabel artistLabel = new JLabel("Artist: " + musique.artist);
-            JLabel genreLabel = new JLabel("Album: " + musique.genre);
-            JLabel albumLabel = new JLabel("Album: " + musique.album);
+    	            JLabel titleLabel = new JLabel("Title: " + musique.titre);
+    	            JLabel artistLabel = new JLabel("Artist: " + musique.artist);
+    	            JLabel genreLabel = new JLabel("Album: " + musique.genre);
+    	            JLabel albumLabel = new JLabel("Album: " + musique.album);
+    	            /**titleLabel.
+    	            artistLabel.
+    	            genreLabel.
+    	            albumLabel.**/
+    	            
+    	            JPanel itemPanel = new JPanel(new BorderLayout());
 
-            JPanel itemPanel = new JPanel(new BorderLayout());
+    	            itemPanel.add(imageLabel, BorderLayout.WEST);
 
-            itemPanel.add(imageLabel, BorderLayout.WEST);
+    	            JPanel textPanel = new JPanel(new GridLayout(3, 1));
+    	            textPanel.add(titleLabel);
+    	            textPanel.add(artistLabel);
+    	            textPanel.add(albumLabel);
+    	            itemPanel.add(textPanel, BorderLayout.CENTER);
 
-            JPanel textPanel = new JPanel(new GridLayout(3, 1));
-            textPanel.add(titleLabel);
-            textPanel.add(artistLabel);
-            textPanel.add(albumLabel);
-            itemPanel.add(textPanel, BorderLayout.CENTER);
-
-            panel.add(itemPanel);
-        }
-        panel.revalidate(); // Mettre à jour le panneau
-        panel.repaint(); // Redessiner le panneau
-    }
+    	            panel.add(itemPanel);
+    	        }
+    	    }
+    	    panel.revalidate(); // Mettre à jour le panneau
+    	    panel.repaint(); // Redessiner le panneau
+    	}
 }
